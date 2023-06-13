f875a51dadd695ef76b0b16fe98a66f5648a0a37
BhavikaMaharshi10 <136457063+BhavikaMaharshi10@users.noreply.github.com>
2023-06-13 17:36:52 +0530

    Final code
    E-commerce mern stack app

---
+++ routes/authRoute.js
@@ -0,0 +1,45 @@
+import express from "express";
+import { registerController,loginController,testController, forgotPasswordController, updateProfileController, getOrdersController, getAllOrdersController, orderStatusController, } from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+//router object
+const router =express.Router()
+
+//routing
+//REGISTER ||METHOD POST
+router.post("/register", registerController);
+
+//Login || POST request
+router.post("/login",loginController);
+
+//forgot password || POST
+router.post('/forgot-password',forgotPasswordController)
+
+//test route
+router.get("/test",requireSignIn,isAdmin,testController);
+
+//Protected user route auth
+router.get('/user-auth', requireSignIn, (_req,res) => {
+    res.status(200).send({ok:true});
+});
+//Protected admin route auth
+router.get('/admin-auth', requireSignIn,isAdmin, (_req,res) => {
+    res.status(200).send({ok:true});
+});
+//update profile
+router.put("/profile", requireSignIn, updateProfileController);
+
+//orders
+router.get("/orders", requireSignIn, getOrdersController);
+
+//all orders
+router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
+
+// order status update
+router.put(
+  "/order-status/:orderId",
+  requireSignIn,
+  isAdmin,
+  orderStatusController
+);
+
+export default router;
